e43314fe8803689e3c0385c50d012bde6f50637a
eburen <evrenxx34@gmail.com>
2023-11-11 02:14:29 +0900

    import React

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        <a>Contact</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
