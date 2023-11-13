90e1dae49a47d8560c8009e77dc8b9da5d7a0aae
eburen <evrenxx34@gmail.com>
2023-11-13 23:59:15 +0900

    Refactor CSS and update page title

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p>Copyright © 2023 by Evren</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
